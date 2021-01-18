375cb6e4f42c96d7ca3487a00d656d355fd329b6
Shade Undertree <72354637+ShadeUndertree@users.noreply.github.com>
2021-01-17 21:40:00 -0800

    build: add pypi packaging

---
+++ README.md
@@ -0,0 +1,19 @@
+# EVM Assembly Utilities
+
+Utility classes and functions for working with EVM (Ethereum Virual Machine) forks and their opcodes.
+
+## Installation
+
+```bash
+pip install evm-asm
+```
+
+## Contributiing
+
+```bash
+$ pip install -e .[dev]
+```
+
+```bash
+$ pytest
+```
---
+++ setup.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+extras_require = {
+    "test": [
+        "pytest==6.2.1",
+        "pytest-xdist",
+        "pytest-coverage",
+    ],
+    "lint": [
+        "black==20.8b1",
+        "flake8==3.8.4",
+        "isort>=5.7.0,<6",
+        "mypy==0.790",
+        "pydocstyle>=5.1.1,<6",
+    ],
+    "doc": [
+        "Sphinx>=3.4.3,<4",
+        "sphinx_rtd_theme>=0.5.1",
+    ],
+    "dev": [
+        "py-evm==0.3.0a20",
+        "pytest-watch>=4.2.0,<5",
+        "wheel",
+        "twine",
+        "ipython",
+    ],
+}
+
+extras_require["dev"] = (
+    extras_require["dev"]
+    + extras_require["test"]  # noqa: W504
+    + extras_require["lint"]  # noqa: W504
+    + extras_require["doc"]  # noqa: W504
+)
+
+with open("./README.md") as readme:
+    long_description = readme.read()
+
+setup(
+    name="evm-asm",
+    version="0.0.0",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Shade Undertree",
+    author_email="shadeundertree@gmail.com",
+    url="https://github.com/ShadeUndertree/evm-asm",
+    include_package_data=True,
+    python_requires=">=3.6, <4",
+    install_requires=[],
+    extras_require=extras_require,
+    py_modules=["evm_asm"],
+    license="Apache License 2.0",
+    zip_safe=False,
+    keywords="ethereum",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
