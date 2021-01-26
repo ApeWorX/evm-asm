22e6a749767f921544b62ee592e4f5c10c457676
Just some guy <3859395+fubuloubu@users.noreply.github.com>
2021-01-25 19:20:48 -0500

    test: add check for # of opcodes

--- tests/test_opcodes.py
+++ tests/test_opcodes.py
@@ -3,6 +3,7 @@ from evm_asm.forks import get_version
 
 def test_forks(fork_name, fork_expected_opcodes):
     fork_opcodes = get_version(fork_name)
+    assert len(fork_opcodes) == len(fork_expected_opcodes)
     for opcode_value, opcode in fork_expected_opcodes.items():
         assert (
             getattr(fork_opcodes, opcode.mnemonic.upper())
