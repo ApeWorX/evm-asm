06dc5b85756fd0676f9797a41e1c45817d3a671c
Just some guy <3859395+fubuloubu@users.noreply.github.com>
2021-03-07 11:28:29 -0500

    chore: upgrade repo structure to project template

--- setup.py
+++ setup.py
@@ -1,44 +1,51 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages  # type: ignore
-
+from setuptools import find_packages, setup  # type: ignore
 
 extras_require = {
-    "cbor": ["cbor==5.2.0"],
-    "test": [
+    "test": [  # `test` GitHub Action jobs uses this
+        "pytest>=6.0,<7.0",  # Core testing package
+        "pytest-xdist",  # multi-process runner
+        "pytest-cov",  # Coverage analyzer plugin
+        # Needed for testing
         "py-evm==0.3.0a20",
-        "pytest==6.2.1",
-        "pytest-xdist",
-        "pytest-coverage",
         "web3<6.0",
     ],
     "lint": [
-        "black==20.8b1",
-        "flake8==3.8.4",
-        "isort>=5.7.0,<6",
-        "mypy==0.790",
-        "pydocstyle>=5.1.1,<6",
+        "black>=20.8b1,<21.0",  # auto-formatter and linter
+        "mypy>=0.800,<1.0",  # Static type analyzer
+        "flake8>=3.8.3,<4.0",  # Style linter
+        "isort>=5.7.0,<6.0",  # Import sorting linter
     ],
     "doc": [
-        "Sphinx>=3.4.3,<4",
-        "sphinx_rtd_theme>=0.5.1",
+        "Sphinx>=3.4.3,<4",  # Documentation generator
+        "sphinx_rtd_theme>=0.1.9,<1",  # Readthedocs.org theme
+        "towncrier>=19.2.0, <20",  # Generate release notes
     ],
     "ethervm": ["flask", "flask-table", "web3<6.0"],
+    "release": [  # `release` GitHub Action job uses this
+        "setuptools",  # Installation tool
+        "setuptools-scm",  # Installation tool
+        "wheel",  # Packaging tool
+        "twine",  # Package upload tool
+    ],
     "dev": [
-        "pytest-watch",
-        "wheel",
-        "twine",
-        "ipython",
+        "commitizen",  # Manage commits and publishing releases
+        "pytest-watch",  # `ptw` test watcher/runner
+        "IPython",  # Console for interacting
+        "ipdb",  # Debugger (Must use `export PYTHONBREAKPOINT=ipdb.set_trace`)
     ],
 }
 
-extras_require["test"] = extras_require["test"] + extras_require["cbor"]
+# NOTE: `pip install -e .[dev]` to install package
 extras_require["dev"] = (
-    extras_require["dev"]
-    + extras_require["test"]  # noqa: W504
-    + extras_require["lint"]  # noqa: W504
-    + extras_require["doc"]  # noqa: W504
+    extras_require["test"]
+    + extras_require["lint"]
+    + extras_require["doc"]
+    + extras_require["ethervm"]
+    + extras_require["release"]
+    + extras_require["dev"]
 )
 
 with open("./README.md") as readme:
@@ -46,29 +53,34 @@ with open("./README.md") as readme:
 
 setup(
     name="evm-asm",
-    version="0.0.0",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Shade Undertree",
     author_email="shadeundertree@gmail.com",
-    url="https://github.com/ShadeUndertree/evm-asm",
+    url="https://github.com/ApeWorX/evm-asm",
     include_package_data=True,
     python_requires=">=3.6, <4",
     install_requires=[],
     extras_require=extras_require,
     py_modules=["evm_asm"],
-    license="Apache License 2.0",
+    license="Apache-2.0",
     zip_safe=False,
     keywords="ethereum",
     packages=find_packages(exclude=["tests", "tests.*"]),
+    package_data={"evm_asm": ["py.typed"]},
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
